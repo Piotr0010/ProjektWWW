641c5b678b30aa92a37991c26c438758cdaaeb95
Yastor0001 <65192722+Yastor-py@users.noreply.github.com>
2022-05-21 17:12:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ function commander(cmd) {
       break;
     
     case "sudo":
-      addLine("Oh no, you're not admin...", "color2", 80);
+      addLine("Oh no, you're not an admin...", "color2", 80);
       setTimeout(function() {
         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
       }, 1000); 
