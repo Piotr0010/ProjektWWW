f6dc42aa34c6bfcba4a257681f3e7b2772c57ba4
Yastor0001 <65192722+Yastor-py@users.noreply.github.com>
2022-05-21 15:48:17 +0200

    0.1

---
+++ termianlweb/js/commands.js
@@ -0,0 +1,105 @@
+var youtube = "https://www.youtube.com";
+var twitter = "https://www.twitter.com";
+var password = "papiezgituwa";
+var linkedin = "https://www.linkedin.com";
+var instagram = "https://www.instagram.com";
+var github = "https://github.com";
+
+
+whois = [
+  "<br>",
+  "Serwer poświęcony egzaminom zawodowym na kierunku technik informatyk jak i maturze z informatyki.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Yastor.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "error",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">play</span>           Włącz muzykę',
+  '<span class="command">mute</span>           Wyłącz muzykę',
+  '<span class="command">video</span>          Włącz wideo w tle',
+  '<span class="command">no video</span>       Wyłącz wideo w tle',
+  '<span class="command">discord</span>        Serwer discord',
+  '<span class="command">dlakogo</span>        Dla kogo jest to serwer?',
+  '<span class="command">whoami</span>         Kim jestem?',  
+  '<span class="command">social</span>         Sociale',
+  '<span class="command">secret</span>         Znajdz haslo',
+  '<span class="command">projects</span>       Zobacz projekty',
+  '<span class="command">history</span>        Historia komend',
+  '<span class="command">help</span>           Przecież wiesz',
+  '<span class="command">email</span>          Brak',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Pokaż banner',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Yastor test terminalWeb.</span>',
+  
+  "                                            __                   __            __                      ",
+  "    __  __  ____   __  __   _____          / /  ____ _   _____  / /_          / /_   ____     ____   ___ ",
+  "   / / / / / __ \\ / / / /  / ___/         / /  / __ `/  / ___/ / __/         / __ \\ / __ \\   / __ \\ / _ \\",
+  "  / /_/ / / /_/ // /_/ /  / /            / /  / /_/ /  (__  ) / /_          / / / // /_/ /  / /_/ //  __/",
+  "  \\__, /  \\____/ \\__,_/  /_/            /_/   \\__,_/  /____/  \\__/         /_/ /_/ \\____/  / .___/ \\___/ ",
+  "  /___/                                                                                   /_/         ",
+ 
+  "                             _____    _                                   _ ",
+  "                            |  __ \\  (_)                                 | |",
+  "                            | |  | |  _   ___    ___    ___    _ __    __| |",
+  "                            | |  | | | | / __|  / __|  / _ \\  | '__|  / _` |",
+  "                            | |__| | | | \\__ \\ | (__  | (_) | | |    | (_| |",
+  "                            |_____/  |_| |___/  \\___|  \\___/  |_|     \\____|",
+  "  ",
+  "  ",
+  
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  
+
+];
+
+discord = [
+  '<p id = "inne"><center><pre class="links"><a id="links" href="https://discord.gg/RJMZQEC" target="_blank">https://discord.gg/RJMZQEC</a>\n\n<a id="links" target="_blank" class="ee" href="https://discord.gg/RJMZQEC">EE.08, EE.09, INF.02, INF03, MATURY</a> </pre></p>',
+  'Egzaminy zawodowe IT/ Matury/',
+  '├─ staff/',
+  '├─ info/',
+  '│  ├─ regulamin',
+  '│  ├─ co-było-na-egzaminie',
+  '│  ├─ ogłoszenia',
+  '├─ kanały tekstowe/',
+  '│  ├─ ogólny',
+  '│  ├─ pytania',
+  '├─ EE.09/ INF.03/',
+  '│  ├─ kalały-tekstowe',
+  '├─ EE.08 / INF.03/',
+  '│  ├─ kanały-tekstowe',
+  '<br>',
+
+];
---
+++ termianlweb/js/main.js
@@ -0,0 +1,277 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer"); 
+var textarea = document.getElementById("texter"); 
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function() {
+  
+  loopLines(banner, "", 80);
+  
+  
+  textarea.focus();
+  play();
+}, 100);
+addLine(discord[0], "no-animation", 1200);
+//czeka na input
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%cEo ",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+console.log();
+
+//Po kliknieciu
+
+function enterKey(e) {
+  console.log("enterKey"+e.keyCode);
+  // let keyCode = e.keyCode;
+  // let chrCode = keyCode - 48 * Math.floor(keyCode / 48);
+  // let chr = String.fromCharCode((96 <= keyCode && keyCode <= 105) ? keyCode-48 : keyCode);
+  // command.innerHTML += chr;
+  //TODO mozliwosc pisania bez klikania w pole tekstowe
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  //jesli tryb hasla
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) { // jesli dobre haslo
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) { //po enterze obsluga poprawnosi hasla
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Wrong password", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML); //lista uzytych
+      git = commands.length; // ile uzyto komend
+      addLine("visitor@fkcodes.com:~$ " + command.innerHTML, "no-animation", 0);
+      console.log("visitor@fkcodes.com:~$ " + command.innerHTML);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    //dzialanie strzalek
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "color2 margin", 80);
+      break;
+    case "dlakogo":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    
+    case "sudo":
+      addLine("Oh no, you're not admin...", "color2", 80);
+      setTimeout(function() {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+      }, 1000); 
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "projects":
+      loopLines(projects, "color2 margin", 80);
+      break;
+    case "password":
+      addLine("<span class=\"inherit\"> Lol! You're joking, right? You\'re gonna have to try harder than that!đ</span>", "error", 100);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "color2", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+    case "email":
+      addLine('Brak danych', "color2", 80);
+      newTab(email);
+      break;
+    case "play":
+        addLine('Playing music...', "color2", 80);
+        play2();
+      break;
+    case "no video":
+        addLine("Wyłączanie wideo...", "color2", 80);
+        document.getElementById("full").style="filter:opacity(0%);";
+        document.getElementById("window").style = "background: rgba(33, 29, 27, 1);";
+      break;
+    case "video":
+        addLine("Włączanie wideo...", "color2", 80);
+        document.getElementById("full").style="filter:opacity(70%);";
+        document.getElementById("window").style = "background: rgba(0, 0, 0, 0.705);";
+      break;
+    case "mute":
+        addLine('Stopping the music...', "color2", 80);
+        mute();
+        
+      break;
+    case "home":
+      
+      loopLines(banner, "no-animation", 80);
+      addLine(discord[0], "no-animation", 1100);
+      textarea.focus();
+      break;
+    case "clear":
+      setTimeout(function() {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "discord":
+      addLine('Loading...', "color2", 80);
+      setTimeout(function() {
+  
+        loopLines(discord, "no-animation", 130);
+        
+        
+        textarea.focus();
+        play();
+      }, 1300);
+      
+      //addLine(discord[0], "no-animation", 1100);
+      textarea.focus();
+      break;
+    
+    // socials
+    case "youtube":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "linkedin":
+      addLine("Opening LinkedIn...", "color2", 0);
+      newTab(linkedin);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "github":
+      addLine("Opening GitHub...", "color2", 0);
+      newTab(github);
+      break;
+    default:
+      addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>.</span>", "error", 100);
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function() {
+    window.open(link, "_blank");
+  }, 500);
+}
+//dodanie linii
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function() {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.getElementById("window").offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function(item, index) {
+    addLine(item, style,  index * time); //kazdy element tablicy pojawia sie w czasie po sobie
+  });
+}
+
+
+var  audioeo = document.getElementById("audio");
+function play(){
+    if(document.getElementById("audio").muted===true){
+        document.getElementById('audio').play()
+        
+        document.getElementById("audio").muted=false
+    }
+    else{
+        document.getElementById('bnt').style='background-image: url("mute.png");'
+        document.getElementById("audio").pause()
+        document.getElementById("audio").muted=true
+        
+    }
+    
+}
+function play2(){
+
+    document.getElementById('audio').play()
+    
+    document.getElementById("audio").muted=false;
+    
+    
+}
+function mute(){
+
+  document.getElementById('audio').play()
+  
+  document.getElementById("audio").muted=true;
+  
+  
+}
